c476d2ff1e297cfb9107d4c599150d60058651ea
Paul Anderson <paul.anderson@uberops.com>
2017-05-06 20:07:48 -0400

    test expected item structure after fetch

--- test/build/fetchItems.js
+++ test/build/fetchItems.js
@@ -4,6 +4,9 @@ const tap = require('tap')
 tap.test('fetchItems should fetch an array of items', t => {
   return fetchItems()
   .then(items => {
-    t.type(items, Array)
+    t.type(items, Array, 'items should be in an array')
+    t.notEqual(typeof items[0].type, 'undefined', 'item objects should have a type property')
+    t.notEqual(typeof items[0].name, 'undefined', 'item objects should have a name property')
+    t.notEqual(typeof items[0].meta, 'undefined', 'item objects should have a meta property')
   })
 })

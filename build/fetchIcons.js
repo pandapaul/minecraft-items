3c9a9e403ea8729ff5aaf959420e6b7620baadec
Paul Anderson <paul.anderson@uberops.com>
2017-05-12 11:24:35 -0400

    :sparkles: Add fetchIcons. Creates an object containing base64-encoded PNGs keyed by item id.

---
+++ build/fetchIcons.js
@@ -0,0 +1,24 @@
+const superagent = require('superagent')
+const binaryParser = require('superagent-binary-parser')
+const AdmZip = require('adm-zip')
+
+const pathToId = (path) => {
+  const matched = path.match(/^(\d+)-(\d+)\.png$/)
+  return `${matched[1]}:${matched[2]}`
+}
+
+module.exports = () => {
+  const icons = {}
+
+  return superagent.get('http://minecraft-ids.grahamedgecombe.com/items.zip')
+  .buffer()
+  .parse(binaryParser)
+  .then(res => res.body)
+  .then(buffer => new AdmZip(buffer))
+  .then(zip => zip.getEntries())
+  .then(entries => entries.forEach(entry => {
+    const itemId = pathToId(entry.entryName)
+    icons[itemId] = entry.getData().toString('base64')
+  }))
+  .then(() => icons)
+}
---
+++ test/build/fetchIcons.js
@@ -0,0 +1,10 @@
+const fetchIcons = require('../../build/fetchIcons')
+const tap = require('tap')
+
+tap.test('fetchIcons should fetch an object of icons keyed by id', t => {
+  return fetchIcons()
+  .then(icons => {
+    t.type(icons, 'object', 'icons should be in an object')
+    t.type(typeof icons['2:0'].type, 'string', 'icons should be represented as strings and keyed by id')
+  })
+})

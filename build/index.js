740cf53aa35a26ab3a9ba3bcbe85e3865c27fedc
Paul Anderson <paul.anderson@uberops.com>
2017-05-13 21:35:37 -0400

    :sparkles: Fetch and combine icons in build process.

--- build/index.js
+++ build/index.js
@@ -1,10 +1,16 @@
 const fetchItems = require('./fetchItems')
+const fetchIcons = require('./fetchIcons')
+const combineItemsAndIcons = require('./combineItemsAndIcons')
 const mapFetchedItems = require('./mapFetchedItems')
 const objectifyMappedItems = require('./objectifyMappedItems')
 const writeJsonFiles = require('./writeJsonFiles')
 const path = require('path')
 
-module.exports = fetchItems()
-.then(mapFetchedItems)
+module.exports = Promise.all([
+  fetchItems(),
+  fetchIcons()
+])
+.then(([items, icons]) => [mapFetchedItems(items), icons])
+.then(combineItemsAndIcons)
 .then(objectifyMappedItems)
 .then(items => writeJsonFiles(items, path.join(__dirname, '../data')))
